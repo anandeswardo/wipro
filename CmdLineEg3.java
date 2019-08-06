fb92c1cb40bc45dd5f6c0dac849351bcd373e2e8
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-08-06 18:36:52 +0530

    Create CmdLineEg3.java

---
+++ CmdLineEg3.java
@@ -0,0 +1,8 @@
+public class CmdLineEg3
+{
+public static void main(String args[]) 
+{
+int sum=Integer.parseInt(args[0])+Integer.parseInt(args[1]);  
+System.out.println("the sum of "+args[0]+" and "+args[1]+" is "+ sum);
+}
+}
